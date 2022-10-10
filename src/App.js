03b235826382225e305fca360f12f4c4bcba16de
amertoukan <toukan.amer@gmail.com>
2022-10-10 16:26:20 -0700

    router ready

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './App.css';
 import {Container } from 'react-bootstrap'
-import { AuthProvider } from './contexts/AuthContext'
 import __Navbar from "./components/router";
 
 function App() {
   
   return (
-    <AuthProvider>
-
-
     <Container>
         <__Navbar/>
     </Container>
-
-    </AuthProvider>
 
   );
 }
--- src/components/router.jsx
+++ src/components/router.jsx
@@ -3,23 +3,28 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, 
         Routes, 
         Route, 
+        Switch,
         Navigate } from "react-router-dom";
 import Home from '../pages/Home';
 import Store from '../pages/Purchase';
 import Referral from '../pages/Referral';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
+import { AuthProvider } from '../contexts/AuthContext'; 
 
 export default function __Navbar(){
     
 return(
     <Router>
+        <AuthProvider>
         <Routes>
-            <Route path="/" element={<Home/>}/>
+            <Route exact path="/" element={<Home/>}/>
             <Route path="/store" element={<Store/>}/>
             <Route path="/referral" element={<Referral/>}/>
             <Route path="/signin" element={<Signin/>}/>
             <Route path="/signup" element={<Signup/>}/>
         </Routes>
+        </AuthProvider>
+     
     </Router>
 )}
