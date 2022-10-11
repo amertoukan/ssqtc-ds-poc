04a1bb94c12a2bb7169c1047a21aed8ec546438c
amertoukan <toukan.amer@gmail.com>
2022-10-10 17:33:37 -0700

    navbar fixed and image renders from google

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'; 
+import { useAuth } from '../contexts/AuthContext'; 
+
 
 export default function Navbar({signedIn}) {
     console.log(signedIn)
-    const div = (signedIn ?   (
+    const { currentUser } = useAuth(); 
+    const div = (currentUser !== null ?   (
         <div className="nav">
              <Link to="/">Home</Link>
             <Link to="/store">Store</Link>
             <Link to="/referral">Referral</Link>
+            <Link to="/settings">Settings</Link>
           
            
       </div>
@@ -17,6 +21,7 @@ export default function Navbar({signedIn}) {
         <Link to="/">Home</Link>
         <Link to="/signup">Sign Up</Link>
         <Link to="/signin">Sign In</Link>
+      
         </div>
       ))
     return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,6 @@ export default function Home() {
   return (
     <div>
         <Navbar signedIn={false}/>
-        <img src={backgroundLogo} className="img"/></div>
+        <img src={"https://d4.alternativeto.net/oMJVT_C2tHWv0k0rhV4HSYHLxqQNkOnQG2WlIR2-iYA/rs:fit:1200:1200:0/g:ce:0:0/YWJzOi8vZGlzdC9zL3JlZmVycmFsLXNhYXNxdWF0Y2hfMTkzMzgwX2Z1bGwucG5n.jpg"} className="img"/></div>
   )
 }
